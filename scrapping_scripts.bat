acd0128081b2ab29cad3dda31b6c42e3388fd796
Dmitrijs Livinskis <dlivinskis@gmail.com>
2023-12-07 19:22:19 +0200

    .bat file for orchestrating all scrapping scripts and database launch

---
+++ scrapping_scripts.bat
@@ -0,0 +1,16 @@
+w:
+cd W:\Coding\PythonProjects\Main_Python_Project\venv\Scripts
+call activate
+cd ../..
+cd ../..
+cd W:\Coding\PythonProjects\Main_Python_Project
+python SS.LV-Cars.py
+python Riga_Riga_region_appartments.py
+python SS.LV_Apartments_Daugavpils.py
+python Check_if_files_exist.py
+cd ../..
+cd ../..
+cd W:\Coding\PostgreSQL\16\bin
+pg_ctl start -D "W:\Coding\PostgreSQL\16\data"
+cd W:\Coding\PythonProjects\Main_Python_Project
+python job_scrapping.py
